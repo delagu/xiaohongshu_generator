55475e0b6a3f67dc9f70f533c3b5d6bed24269e5
delagu <50522230+delagu@users.noreply.github.com>
2026-03-27 16:42:00 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,28 +1,27 @@
-from langchain_core.chat_history import InMemoryChatMessageHistory
-from langchain_core.runnables import RunnableWithMessageHistory
+from langchain_core.output_parsers import PydanticOutputParser
+from langchain_core.prompts import ChatPromptTemplate
+from prompt_template import system_template_text, user_template_text
 from langchain_openai import ChatOpenAI
-from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from xiaohongshu_model import Xiaohongshu
 import os
 
-def get_session_history(session_id:str,store):
-    if session_id not in store:
-        store[session_id] = InMemoryChatMessageHistory()
-    return store[session_id]
-
-def get_chat_response(prompt,store,session_id,api_key):
+def generate_xiaohongshu(theme, api_key):
+    prompt = ChatPromptTemplate.from_messages([
+        ("system", system_template_text),
+        ("user", user_template_text)
+    ])
     model = ChatOpenAI(
         base_url="https://dashscope.aliyuncs.com/compatible-mode/v1",
         model="qwen-turbo",
         api_key=api_key
     )
-    prompt_template = ChatPromptTemplate.from_messages([
-        MessagesPlaceholder(variable_name="history"),
-        ("human","{input}")
-    ])
-    basic_chain = prompt_template | model
-    chain = RunnableWithMessageHistory(basic_chain,lambda sid:get_session_history(sid,store),
-                                       input_messages_key="input",
-                                       history_messages_key="history")
-    response = chain.invoke({"input":prompt},{"configurable":{"session_id":session_id}})
-    return response.content
-    print(response2)
+    output_parser = PydanticOutputParser(pydantic_object=Xiaohongshu)
+    chain = prompt | model | output_parser
+    result = chain.invoke({
+        "parser_instructions": output_parser.get_format_instructions(),
+        "theme": theme
+    })
+    return result
+
+if __name__ == '__main__':
+    print(generate_xiaohongshu("大模型", os.getenv("DASHSCOPE_API_KEY")))
